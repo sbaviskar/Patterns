408b0e95f81b1636e8d40d4ac806f1ec575c1776
Swapnil Baviskar <55089869+swapnil9baviskar@users.noreply.github.com>
2021-02-24 23:53:56 +0530

    Create Pattern78.java

---
+++ Pattern78.java
@@ -0,0 +1,28 @@
+/*
+		*    **********    *
+		**   ****  ****   **
+		***  ***    ***  ***
+		**** **      ** ****
+		******        ******
+
+
+*/
+public class Pattern78 {
+
+	public static void main(String[] args) {
+|
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 20; j++) {
+
+				if( j - i <= 0 || j + i <= 9 && j > 4 || j - i >= 10 && j < 15 || j +i >= 19 ) {
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
